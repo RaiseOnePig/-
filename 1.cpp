8a63318b31686bdda8917d706ab05ca2b7f51b2d
RaiseOnePig <1134960129@qq.com>
2022-09-20 19:30:37 +0800

    master

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode{
+    int val;
+    struct ListNode *next;
+    ListNode(int x) :
+        val(x), next(NULL){}
+};
+class Solution{
+public:
+    ListNode *FindFirstCommonNode(ListNode *pHead1, ListNode *pHead2){
+        
+    }
+};
+
+
+int main(){
+    return 0;
+}
